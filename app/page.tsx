7846a1645f5b2f21531a9f350b86c39ec1edfb6b
Aananda-git <bhusal.aananda1@gmail.com>
2025-11-28 19:19:13 +0545

    feat: importing all component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,24 @@
-import { Button } from "@/components/ui/button";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Hi! I am Aananda
-            <Button>Click me</Button>
-          </h1>
-        </div>
-      </main>
-    </div>
+    <>
+      <Navbar />
+      <section id="hero"><Hero /></section>
+      <section id="about"><About /></section>
+      <section id="experience"><Experience /></section>
+      <section id="skills"><Skills /></section>
+      <section id="projects"><Projects /></section>
+      <section id="contact"><Contact /></section>
+      <Footer />
+    </>
   );
 }
